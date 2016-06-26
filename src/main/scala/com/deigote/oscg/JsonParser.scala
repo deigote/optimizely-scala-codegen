22b2ea12dfe3ef401e0bd4e64ece116a6f49bf60
deigote <diego@toharia.com>
2016-06-26 12:15:38 +0200

    (refactor) JSON parsing extract methods' signature do not receive an option
    Forcing an option does not always make sense and prevent monadic + fluent api style

--- src/main/scala/com/deigote/oscg/JsonParser.scala
+++ src/main/scala/com/deigote/oscg/JsonParser.scala
@@ -1,9 +1,6 @@
 package com.deigote.oscg
 
-import java.nio.charset.StandardCharsets
-
-import scala.io.Source
-import scala.util.parsing.json.{JSONType, JSONArray, JSON, JSONObject}
+import scala.util.parsing.json.{JSON, JSONArray, JSONObject}
 
 object JsonParser {
 
@@ -11,15 +8,19 @@ object JsonParser {
 	private val jsonArray: Class[JSONArray] = classOf[JSONArray]
 
 	def parseObject(definitionAsJson: String): JSONObject =
-		(JSON.parseRaw _ andThen extractObject)(definitionAsJson)
+		JSON
+			.parseRaw(definitionAsJson)
+			.flatMap(extractObject)
 			.getOrElse(throw new IllegalArgumentException(s"Couldn't parse ${definitionAsJson} as a JSON object"))
 
-	def extractObject(raw: Option[Any]) = extract[JSONObject](raw, jsonObject)
-	def extractArray(raw: Option[Any]) = extract[JSONArray](raw, jsonArray)
+	def extractObject(raw: Any): Option[JSONObject] =
+		extract[JSONObject](raw, jsonObject)
+	def extractArray(raw: Any): Option[JSONArray] =
+		extract[JSONArray](raw, jsonArray)
 
-	private def extract[C](raw: Option[Any], classToExtract: Class[C]): Option[C] =
-		raw.flatMap(json => json.getClass match {
+	private def extract[C](json: Any, classToExtract: Class[C]): Option[C] =
+		json.getClass match {
 			case `classToExtract` => Some(json.asInstanceOf[C])
 			case _ => None
-		})
+		}
 }
