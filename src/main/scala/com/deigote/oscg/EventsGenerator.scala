326463fdb482ff3323442d106fbef403005b424a
deigote <diego@toharia.com>
2016-06-26 12:28:29 +0200

    Code for events can be generated

---
+++ src/main/scala/com/deigote/oscg/EventsGenerator.scala
@@ -0,0 +1,36 @@
+package com.deigote.oscg
+
+import scala.collection.JavaConversions.{seqAsJavaList, mapAsJavaMap}
+import scala.util.parsing.json.{JSONArray, JSONObject}
+
+object EventsGenerator extends CodeGenerator {
+
+	override val templateName: String = "Events"
+	private val jsonArray = classOf[JSONArray]
+
+	override def model(rootObject: JSONObject): Map[String, Any] =
+		Map(
+			"events" -> seqAsJavaList(getEventsAsList(rootObject)),
+			"className" -> "Event"
+		)
+
+	def getEventsAsList(rootObject: JSONObject) =
+		rootObject
+			.obj
+			.get("events")
+			.flatMap(JsonParser.extractArray)
+			.getOrElse(throw new IllegalArgumentException("couldn't find a JSON array when getting the 'events' object"))
+			.list
+			.flatMap(JsonParser.extractObject)
+	  		.map(_.obj)
+		   .map(addClassName _ andThen mapAsJavaMap)
+
+	def addClassName(eventModel: Map[String, Any]): Map[String, Any] =
+		eventModel ++
+			eventModel
+				.get("key")
+				.map(_.toString.capitalize)
+				.map(kebabToCamelCase)
+				.map(className => Map("className" -> className))
+				.getOrElse(Map())
+}
